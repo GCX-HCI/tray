24ce93a4ac4b80559cd826fdafefb66256daf106
Pascal Welsch <pascal.welsch@grandcentrix.net>
2015-08-23 16:23:34 +0200

    added documentation about the TrayStorage.Type

--- library/src/main/java/net/grandcentrix/tray/storage/TrayStorage.java
+++ library/src/main/java/net/grandcentrix/tray/storage/TrayStorage.java
@@ -43,9 +43,31 @@ import java.util.List;
  */
 public class TrayStorage extends ModularizedStorage<TrayItem> {
 
+    /**
+     * The type of the data indicating their backup strategy to the cloud
+     */
     public enum Type {
+        /**
+         * don't use {@link #UNDEFINED} when creating a {@link net.grandcentrix.tray.TrayPreferences}.
+         * It's used internally to import a preference by moduleName without knowing the location
+         * of this preference (user or device). Because of that a undefined TrayStorage lookups the
+         * data in both data stores.
+         * <p>
+         * Because it's not clear where to save data a undefined TrayStorage is only able to read
+         * and delete items. Writing with <code>put()</code> is <b>not</b> allowed.
+         */
         UNDEFINED,
+        /**
+         * the data relates to the user and can be saved in the cloud and restored on another
+         * device
+         */
         USER,
+        /**
+         * the data is device specific like a GCM push token or settings that is important for this
+         * specific device.
+         * <p>
+         * Such data shouldn't saved to the cloud with auto backup since Android Marshmallow.
+         */
         DEVICE
     }
 
@@ -179,7 +201,7 @@ public class TrayStorage extends ModularizedStorage<TrayItem> {
         //noinspection ConstantConditions
         if (key == null) {
             throw new IllegalArgumentException(
-                    "null is not valid. use clear to delete all preferences");
+                    "null is not valid. use clear or wipe to delete all preferences");
         }
         final Uri uri = mTrayUri.builder()
                 .setType(mType)
